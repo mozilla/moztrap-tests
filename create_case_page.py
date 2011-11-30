bd31915a088bb69a3fdec9b47ba83a71017a3d46
Jonny Gerig Meyer <jonny@oddbird.net>
2011-11-30 10:52:29 -0500

    Added test that user can create and delete case.

---
+++ create_case_page.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Case Conductor
+#
+# The Initial Developer of the Original Code is
+# Mozilla Corp.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s): Jonny Gerig Meyer <jonny@oddbird.net>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+from base_page import CaseConductorBasePage
+from datetime import datetime
+
+
+class CaseConductorCreateCasePage(CaseConductorBasePage):
+
+    _page_title = 'Mozilla Case Conductor'
+
+    _name_locator = 'id=id_name'
+    _product_select_locator = 'id=id_product'
+    _description_locator = 'id=id_description'
+    _step1_instruction_locator = 'id=id_steps-0-instruction'
+    _step1_result_locator = 'id=id_steps-0-expected_result'
+    _submit_locator = 'css=#single-case-form .form-actions > button'
+    _case_locator = u'css=#managecases .managelist article.item .title[title="%(case_name)s"]'
+
+    def go_to_create_case_page(self, login=False, user='default'):
+        self.selenium.open('/manage/testcase/add/')
+
+        if login:
+            from login_page import CaseConductorLoginPage
+            login_pg = CaseConductorLoginPage(self.testsetup)
+            login_pg.login(user)
+
+        self.is_the_current_page
+
+    def create_case(self, name='Test Case', product='Test Product', desc='This is a test case', step1_instruction='Test Case step 1 instruction', step1_result='Test Case step 1 expected result'):
+        dt_string = datetime.utcnow().isoformat()
+        case = {}
+        case['name'] = u'%(name)s %(dt_string)s' % {'name': name, 'dt_string': dt_string}
+        case['desc'] = u'%(desc)s created on %(dt_string)s' % {'desc': desc, 'dt_string': dt_string}
+        case['locator'] = self._case_locator % {'case_name': case['name']}
+
+        self.type(self._name_locator, case['name'])
+        self.select(self._product_select_locator, product)
+        self.type(self._description_locator, case['desc'])
+        self.type(self._step1_instruction_locator, step1_instruction)
+        self.type(self._step1_result_locator, step1_result)
+        self.click(self._submit_locator, wait_flag=True)
+
+        return case
---
+++ test_manage_cases_page.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Case Conductor
+#
+# The Initial Developer of the Original Code is
+# Mozilla Corp.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s): Jonny Gerig Meyer <jonny@oddbird.net>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+from create_case_page import CaseConductorCreateCasePage
+from manage_cases_page import CaseConductorManageCasesPage
+from create_product_page import CaseConductorCreateProductPage
+from manage_products_page import CaseConductorManageProductsPage
+from unittestzero import Assert
+
+
+class TestManageCasesPage:
+
+    def create_product(self, mozwebqa):
+        create_product_pg = CaseConductorCreateProductPage(mozwebqa)
+
+        create_product_pg.go_to_create_product_page(login=True)
+        product = create_product_pg.create_product()
+
+        return product
+
+    def delete_product(self, product, mozwebqa):
+        manage_products_pg = CaseConductorManageProductsPage(mozwebqa)
+
+        manage_products_pg.go_to_manage_products_page()
+        manage_products_pg.filter_products_by_name(name=product['name'])
+        manage_products_pg.delete_product(name=product['name'])
+
+    def test_that_user_can_create_and_delete_case(self, mozwebqa):
+        manage_cases_pg = CaseConductorManageCasesPage(mozwebqa)
+        create_case_pg = CaseConductorCreateCasePage(mozwebqa)
+
+        product = self.create_product(mozwebqa)
+
+        create_case_pg.go_to_create_case_page()
+
+        case = create_case_pg.create_case(product=product['name'])
+
+        manage_cases_pg.filter_cases_by_name(name=case['name'])
+
+        Assert.true(manage_cases_pg.is_element_present(case['locator']))
+
+        manage_cases_pg.delete_case(name=case['name'])
+
+        Assert.false(manage_cases_pg.is_element_present(case['locator']))
+
+        self.delete_product(product, mozwebqa)
