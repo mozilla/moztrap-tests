ccbe1aa5f7b6febabcf3979dbdfaf38f33534bd9
Rebecca Billings <rerlin@gmail.com>
2012-05-03 15:51:25 -0700

    Switched manage cases page to webdriver

--- pages/manage_cases_page.py
+++ pages/manage_cases_page.py
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
 from pages.base_page import MozTrapBasePage
 
 
@@ -11,35 +14,27 @@ class MozTrapManageCasesPage(MozTrapBasePage):
 
     _page_title = 'MozTrap'
 
-    _delete_case_locator = u'css=#managecases .itemlist .listitem[data-title="%(case_name)s"] .action-delete'
-    _case_status_locator = u'css=#managecases .itemlist .listitem[data-title="%(case_name)s"] .status-action'
-    _filter_input_locator = 'id=text-filter'
-    _filter_suggestion_locator = u'css=#filter .textual .suggest .suggestion[data-type="name"][data-name="%(filter_name)s"]'
-    _filter_locator = u'css=#filterform .filter-group input[data-name="name"][value="%(filter_name)s"]:checked'
+    _delete_case_locator = (By.CSS_SELECTOR, '#managecases .itemlist .listitem[data-title="%(case_name)s"] .action-delete')
+    _case_status_locator = (By.CSS_SELECTOR, '#managecases .itemlist .listitem[data-title="%(case_name)s"] .status-action')
+    _filter_input_locator = (By.ID, 'text-filter')
+    _filter_suggestion_locator = (By.CSS_SELECTOR, '#filter .textual .suggest .suggestion[data-type="name"][data-name="%(filter_name)s"]')
+    _filter_locator = (By.CSS_SELECTOR, '#filterform .filter-group input[data-name="name"][value="%(filter_name)s"]:checked')
 
     def go_to_manage_cases_page(self):
-        self.selenium.open('/manage/cases/')
+        self.selenium.get(self.base_url + '/manage/cases/')
         self.is_the_current_page
 
     def delete_case(self, name='Test Case'):
-        _delete_locator = self._delete_case_locator % {'case_name': name}
-
-        self.click(_delete_locator)
-        self.wait_for_ajax()
+        find_element(*self._delect_case_locator).click()
 
     def filter_cases_by_name(self, name):
-        _filter_locator = self._filter_locator % {'filter_name': name.lower()}
-        _filter_suggestion_locator = self._filter_suggestion_locator % {'filter_name': name}
+        self.selenium.find_element(*self._filter_locator % {'filter_name': name.lower()})
+        self.selenium.find_element(*self._filter_suggestion_locator % {'filter_name': name})
 
-        self.type(self._filter_input_locator, name)
-        self.selenium.type_keys(self._filter_input_locator, name)
-        self.wait_for_element_present(_filter_suggestion_locator)
-        self.click(_filter_suggestion_locator)
-        self.wait_for_element_present(_filter_locator)
-        self.wait_for_ajax()
+        name_field = self.selenium.find_element(*self._filter_input_locator)
+        name_field.send_keys(name)
+
+        self.selenium.find_element(*self._filter_suggestion_locator).click()
 
     def activate_case(self, name='Test Case'):
-        _case_status_locator = self._case_status_locator % {'case_name': name}
-
-        self.click(_case_status_locator)
-        self.wait_for_ajax()
+        find_element(*self._case_status_locator).click()
