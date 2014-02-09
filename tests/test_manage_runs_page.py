772dbbdd476992f25bad34f3b2ee9e127045d2ec
Sasha Kruglov <sashakruglov@hotmail.com>
2014-02-10 00:39:51 +0400

    added test for 'blocked' status

--- tests/test_manage_runs_page.py
+++ tests/test_manage_runs_page.py
@@ -26,3 +26,40 @@ class TestManageRunsPage(BaseTest):
         manage_runs_pg.delete_run(name=run['name'])
 
         Assert.false(manage_runs_pg.is_element_present(*run['manage_locator']))
+
+    @pytest.mark.moztrap(3302)
+    def test_for_edit_active_run_that_includes_suites_to_be_sure_they_are_listed(self, mozwebqa_logged_in, product):
+        #create version
+        version = product['version']
+        #create two test suites
+        suite_a = self.create_suite(mozwebqa_logged_in, product=product, use_API=True, name='suite A')
+        suite_b = self.create_suite(mozwebqa_logged_in, product=product, use_API=True, name='suite B')
+        #create test run
+        suite_order = [suite_b['name'], suite_a['name']]
+        test_run = self.create_run(
+            mozwebqa_logged_in, product=product,
+            version=version, suite_name_list=suite_order)
+
+        manage_runs_pg = MozTrapManageRunsPage(mozwebqa_logged_in)
+        manage_runs_pg.go_to_manage_runs_page()
+        manage_runs_pg.filter_form.filter_by(lookup='name', value=test_run['name'])
+        manage_runs_pg.activate_run(name=test_run['name'])
+        edit_run_pg = manage_runs_pg.go_to_edit_run_page(test_run['name'])
+
+        #assert that multiselect widget is not present thus suites list is readonly
+        Assert.false(
+            edit_run_pg.is_multiselect_widget_present,
+            u'multiselect widget should not be present')
+        #assert that order of suites is correct
+        Assert.equal(
+            suite_order, edit_run_pg.readonly_included_suites,
+            u'suites are listed in wrong order')
+
+        edit_run_pg.save_run()
+        test_run = manage_runs_pg.test_runs[0]
+        test_run.show_details()
+
+        #assert that order of suites is still correct
+        Assert.equal(
+            suite_order, test_run.included_suites,
+            u'suites are listed in wrong order')
